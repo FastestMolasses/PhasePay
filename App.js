1881be69ef28cd7abe50c047c6a4eb246ef38ab9
Abe Malla <abe.malla8@gmail.com>
2018-11-23 01:45:34 -0800

    Added instructions to fix bug

--- App.js
+++ App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { MainTabNav } from './Router';
 
+// Fixing RN debug env
+// react-native upgrade
+// rm -rf /usr/local/var/run/watchman && brew uninstall watchman && brew install watchman
+
+
 export default class App extends React.Component {
     render() {
         return <MainTabNav />;
